9d4844d09e1448db703ed118e385a43ca06589fb
Franzi Horst <franziska.horst-design@web.de>
2018-11-12 10:26:06 +0100

    Actual Version

---
+++ docs/build/precache-manifest.ac3602a18e82c6cdc0bc5fa9bab09fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "954dc766be8e06fc5828",
+    "url": "/static/js/main.954dc766.chunk.js"
+  },
+  {
+    "revision": "8261afbf1949491e631f",
+    "url": "/static/js/1.8261afbf.chunk.js"
+  },
+  {
+    "revision": "954dc766be8e06fc5828",
+    "url": "/static/css/main.40f91202.chunk.css"
+  },
+  {
+    "revision": "70972cafb7e12c98a3fc1e7fc240e5c8",
+    "url": "/index.html"
+  }
+];
