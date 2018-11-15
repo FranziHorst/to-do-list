432b75f5a85a49297774a82d9b19796c12c5643d
Franzi Horst <franziska.horst-design@web.de>
2018-11-15 15:44:08 +0100

    Router implement

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,166 +1,83 @@
-import React, { Component } from 'react'
-import uid from 'uid'
-import ToDoListItem from './ToDoListItem'
-import Input from './Input'
-import Counter from './Counter'
-import Seperator from './Seperator'
-import ToggleButton from './ToggleButton'
-import ProgressBar from './ProgressBar'
-import TextArea from './TextArea'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import React, { Component } from 'react'
 
-console.clear()
+import Home from './Home'
+import Config from './Config'
 
-const Layout = styled.div`
-  box-sizing: border-box;
+const Grid = styled.div`
+  display: grid;
+  grid-template-rows: auto 50px;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   background: linear-gradient(45deg, #0d2843 155px, #2980b9);
-`
 
-const StyleEl = styled.div`
-  width: 436px;
-  position: relative;
-  height: 436px;
-  background: #ffcc00;
-  border-radius: 29% 71% 22% 78% / 57% 72% 28% 43%;
-`
-
-const Container = styled.main`
-  background: white;
-  width: 300px;
-  min-height: 50vh;
-  margin: auto;
-  border-radius: 10px;
-  padding: 15px;
-  position: absolute;
-`
-
-const ListContainer = styled.section`
-  background: #f9f9f9;
-  display: flex;
-  flex-direction: column;
-  margin: 0.5rem;
-`
-
-const Headline = styled.header`
-  text-align: center;
-`
-
-class App extends Component {
-  state = {
-    todos: this.load()
+  nav {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 2px;
   }
 
-  addToDo = text => {
-    this.setState({
-      todos: [{ text, done: false, id: uid() }, ...this.state.todos]
-    })
-  }
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #006194;
+    color: #2980b9;
+    text-decoration: none;
+    border-radius: 10px;
+    font-size: 30px;
+    font-family: 'Patrick Hand', cursive;
+    color: white;
 
-  toggleDone = id => {
-    const { todos } = this.state
-    const index = todos.findIndex(todo => todo.id === id)
-    const newArray = [
-      ...todos.slice(0, index),
-      { ...todos[index], done: !todos[index].done },
-      ...todos.slice(index + 1)
-    ]
-    this.setState({
-      todos: newArray
-    })
-  }
-
-  deleteListItem = id => {
-    const { todos } = this.state
-    const index = todos.findIndex(todo => todo.id === id)
-    const newArray = [...todos.slice(0, index), ...todos.slice(index + 1)]
-    this.setState({
-      todos: newArray
-    })
-  }
-
-  counterToDo = () => {
-    return this.state.todos.filter(item => item.done).length
-  }
-
-  save() {
-    localStorage.setItem('todo-app--todos', JSON.stringify(this.state.todos))
-  }
-
-  load() {
-    try {
-      return JSON.parse(localStorage.getItem('todo-app--todos')) || []
-    } catch (err) {
-      return []
+    &.selected {
+      background: #ffcc00;
+      color: #006194;
     }
   }
+`
 
-  renderOpenTodos() {
-    return this.state.todos
-      .filter(todo => !todo.done)
-      .map(todo => (
-        <ToDoListItem
-          key={todo.id}
-          text={todo.text}
-          isDone={todo.done}
-          click={() => this.toggleDone(todo.id)}
-          deleteEl={() => this.deleteListItem(todo.id)}
-        />
-      ))
+export default class App extends Component {
+  state = {
+    showDoneTodos: false
   }
 
-  renderDoneTodos() {
-    return this.state.todos
-      .filter(todo => todo.done)
-      .map(todo => (
-        <ToDoListItem
-          key={todo.id}
-          text={todo.text}
-          isDone={todo.done}
-          click={() => this.toggleDone(todo.id)}
-          deleteEl={() => this.deleteListItem(todo.id)}
-        />
-      ))
-  }
-
-  determineProgress() {
-    const { todos } = this.state
-    const doneTodos = todos.filter(t => t.done)
-    return doneTodos.length / todos.length
+  toggleShowDonetodos = () => {
+    this.setState({
+      showDoneTodos: !this.state.showDoneTodos
+    })
   }
 
   render() {
-    this.save()
     return (
-      <Layout>
-        <StyleEl />
-        <Container>
-          <ProgressBar percentage={this.determineProgress()} />
-          <Headline>
-            <Counter num={this.counterToDo()} />
-          </Headline>
-          <section>
-            <Input onEnter={this.addToDo} />
-          </section>
-          <ListContainer>
-            <Seperator text="TODO" />
-            {this.renderOpenTodos()}
-            <Seperator text="DONE" />
-            {this.renderDoneTodos()}
-          </ListContainer>
-          <ToggleButton
-            defaultText="Hello"
-            alternativeText="Goodbye"
-            onClick={() => console.log('click')}
-            isDefault={true}
-          />
-          <TextArea />
-        </Container>
-      </Layout>
+      <Router>
+        <Grid>
+          <div>
+            <Route
+              exact
+              path="/"
+              render={() => <Home showDoneTodos={this.state.showDoneTodos} />}
+            />
+
+            <Route
+              path="/config"
+              render={() => (
+                <Config
+                  showDoneTodos={this.state.showDoneTodos}
+                  onToggle={this.toggleShowDonetodos}
+                />
+              )}
+            />
+          </div>
+          <nav>
+            <NavLink exact activeClassName="selected" to="/">
+              Home
+            </NavLink>
+            <NavLink activeClassName="selected" to="/config">
+              Config
+            </NavLink>
+          </nav>
+        </Grid>
+      </Router>
     )
   }
 }
-export default App
---
+++ src/Components/Home.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react'
+import uid from 'uid'
+import ToDoListItem from './ToDoListItem'
+import Input from './Input'
+import Counter from './Counter'
+import Seperator from './Seperator'
+import ToggleButton from './ToggleButton'
+import ProgressBar from './ProgressBar'
+import TextArea from './TextArea'
+import styled from 'styled-components'
+
+console.clear()
+
+const Layout = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(45deg, #0d2843 155px, #2980b9);
+`
+
+const StyleEl = styled.div`
+  width: 436px;
+  position: relative;
+  height: 436px;
+  background: #ffcc00;
+  border-radius: 29% 71% 22% 78% / 57% 72% 28% 43%;
+`
+
+const Container = styled.main`
+  background: white;
+  width: 300px;
+  min-height: 50vh;
+  margin: auto;
+  border-radius: 10px;
+  padding: 15px;
+  position: absolute;
+`
+
+const ListContainer = styled.section`
+  background: #f9f9f9;
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+`
+
+const Headline = styled.header`
+  text-align: center;
+`
+
+class Home extends Component {
+  state = {
+    todos: this.load()
+  }
+
+  addToDo = text => {
+    this.setState({
+      todos: [{ text, done: false, id: uid() }, ...this.state.todos]
+    })
+  }
+
+  toggleDone = id => {
+    const { todos } = this.state
+    const index = todos.findIndex(todo => todo.id === id)
+    const newArray = [
+      ...todos.slice(0, index),
+      { ...todos[index], done: !todos[index].done },
+      ...todos.slice(index + 1)
+    ]
+    this.setState({
+      todos: newArray
+    })
+  }
+
+  deleteListItem = id => {
+    const { todos } = this.state
+    const index = todos.findIndex(todo => todo.id === id)
+    const newArray = [...todos.slice(0, index), ...todos.slice(index + 1)]
+    this.setState({
+      todos: newArray
+    })
+  }
+
+  counterToDo = () => {
+    return this.state.todos.filter(item => item.done).length
+  }
+
+  save() {
+    localStorage.setItem('todo-app--todos', JSON.stringify(this.state.todos))
+  }
+
+  load() {
+    try {
+      return JSON.parse(localStorage.getItem('todo-app--todos')) || []
+    } catch (err) {
+      return []
+    }
+  }
+
+  renderOpenTodos() {
+    return this.state.todos
+      .filter(todo => !todo.done)
+      .map(todo => (
+        <ToDoListItem
+          key={todo.id}
+          text={todo.text}
+          isDone={todo.done}
+          click={() => this.toggleDone(todo.id)}
+          deleteEl={() => this.deleteListItem(todo.id)}
+        />
+      ))
+  }
+
+  renderDoneTodos() {
+    return (
+      <React.Fragment>
+        <Seperator text="DONE" />
+        {this.state.todos
+          .filter(todo => todo.done)
+          .map(todo => (
+            <ToDoListItem
+              key={todo.id}
+              text={todo.text}
+              isDone={todo.done}
+              click={() => this.toggleDone(todo.id)}
+              deleteEl={() => this.deleteListItem(todo.id)}
+            />
+          ))}
+      </React.Fragment>
+    )
+  }
+
+  determineProgress() {
+    const { todos } = this.state
+    const doneTodos = todos.filter(t => t.done)
+    return doneTodos.length / todos.length
+  }
+
+  render() {
+    this.save()
+    return (
+      <Layout>
+        <StyleEl />
+        <Container>
+          <ProgressBar percentage={this.determineProgress()} />
+          <Headline>
+            <Counter num={this.counterToDo()} />
+          </Headline>
+          <section>
+            <Input onEnter={this.addToDo} />
+          </section>
+          <ListContainer>
+            <Seperator text="TODO" />
+            {this.renderOpenTodos()}
+            {this.props.showDoneTodos && this.renderDoneTodos()}
+          </ListContainer>
+
+          <TextArea />
+        </Container>
+      </Layout>
+    )
+  }
+}
+export default Home
