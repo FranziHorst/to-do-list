82ba0aef6fbf8c988adaf9ee9975238625c95fc9
Franzi Horst <franziska.horst-design@web.de>
2018-11-16 21:27:01 +0100

    Now releasen

---
+++ docs/precache-manifest.54d89307de9e8044a30e9d7a13ec4dbe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65649d3bee5f515aab16",
+    "url": "/static/js/main.65649d3b.chunk.js"
+  },
+  {
+    "revision": "cd2959717c908ce7e92f",
+    "url": "/static/js/1.cd295971.chunk.js"
+  },
+  {
+    "revision": "2a56a8a323528fe476f8cc53651e0121",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74aaf6f28a1e119032785de030a078c2.js"
+  "/precache-manifest.54d89307de9e8044a30e9d7a13ec4dbe.js"
 );
 
 workbox.clientsClaim();
